672fc6e462c405119d807cd9eb3e42f2f5b2e0fa
punkstorm28 <mailkeshavmail@yahoo.com>
2016-05-21 16:20:43 +0530

    0.4
    Home Ready to be linked, Linking, Gridview To be made

--- app/src/main/java/com/example/vyomkeshjha/dps_h/Render/WelcomeScreen.java
+++ app/src/main/java/com/example/vyomkeshjha/dps_h/Render/WelcomeScreen.java
@@ -75,13 +75,7 @@ public class WelcomeScreen extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+          if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
